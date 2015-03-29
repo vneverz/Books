e6537af1b941bc7f1b0360e8b9d2f6076426bf28
vneverz <vneverz@gmail.com>
2015-03-29 11:10:03 +0800

    db migration

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150329025451) do
+ActiveRecord::Schema.define(version: 20150329030635) do
 
   create_table "books", force: :cascade do |t|
     t.string   "title"
@@ -19,6 +19,16 @@ ActiveRecord::Schema.define(version: 20150329025451) do
     t.date     "release"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "events", force: :cascade do |t|
+    t.string   "name"
+    t.text     "description"
+    t.boolean  "is_public"
+    t.integer  "capacity"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+    t.string   "status"
   end
 
 end
