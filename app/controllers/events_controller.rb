b5a61eb5b6a1eca48fe66b9d3412cc06e43f8ca8
vneverz <vneverz@gmail.com>
2015-03-29 11:45:47 +0800

    before_action and flash set

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,39 +1,49 @@
 class EventsController < ApplicationController
+  before_action :set_event, :only => [ :show, :edit, :update, :destroy]
   def index
     @events = Event.all
   end
   def new
   @event = Event.new
   end
+
   def create
   @event = Event.new(event_params)
-  @event.save
-
-  redirect_to :action => :index
+    if @event.save
+      redirect_to :action => :index
+    else
+      render :action => :new
+    end
+    flash[:notice] = "event was successfully created"
   end
 
   def show
-    @event = Event.find(params[:id])
     @page_title = @event.name
   end
 
   def edit
-    @event = Event.find(params[:id])
   end
 
   def update
-    @event = Event.find(params[:id])
-    @event.update(event_params)
-
-    redirect_to :action => :show, :id => @event
+    if @event.update(event_params)
+      redirect_to :action => :show, :id => @event
+    else
+      render :action => :edit
+    end
+  flash[:notice] = "event was successfully created"
   end
 
   def destroy
-    @event = Event.find(params[:id])
     @event.destroy
-
     redirect_to :action => :index
+    flash[:alert] = "event was successfully deleted"
+
   end
+
+  def set_event
+    @event = Event.find(params[:id])
+  end
+
   private
 
   def event_params
